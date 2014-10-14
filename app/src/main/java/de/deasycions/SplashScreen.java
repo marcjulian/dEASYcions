fd88b5ffdf162b94f947f603a18ca6522e52e999
gary.grossgarten <gary.grossgarten@googlemail.com>
2014-10-15 00:33:37 +0200

    Added SplashScreen Blank Page(Placeholder) Set StartPage to second Activity Changed background in all Activities

---
+++ app/src/main/java/de/deasycions/SplashScreen.java
@@ -0,0 +1,37 @@
+package de.deasycions;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Gary on 14.10.2014.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                   sleep(4000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+
+                    Intent intent = new Intent("de.deasycions.StartPage");
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
